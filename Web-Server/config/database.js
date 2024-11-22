00176cfafdfe5331d5414347d44049d9f2ea4dfd
giangzinD21 <giangtt.b21vt170@stu.ptit.edu.vn>
2024-11-22 10:25:06 +0700

    22/11

--- Web-Server/config/database.js
+++ Web-Server/config/database.js
@@ -2,10 +2,8 @@ const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
   host: "localhost",
-  port: 3307, //default 3306
   user: "root",
   password: "123456",
-  database: "maybay",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- Web-Server/routes/web.js
+++ Web-Server/routes/web.js
@@ -1,67 +1,18 @@
 const express = require("express");
+
 const {
   getindex,
-  getlogin,
-  getintro,
-  getsignin,
-  getsupport,
-  getpayment,
-  getResults,
-  getmisspassword,
-  getsearch,
-  gethome,
-  getadmin,
-  getUpdateUser,
-  getUpdateProduct,
-  postCreateUser,
-  postCheckUser,
-  postUpdateUser,
-  postDeleteUser,
-  postCreateProduct,
-  postUpdateProduct,
-  postDeleteProduct,
-  postSearchProduct,
+  postCheckCustomer,
+  postCreateCustomer,
 } = require("../controllers/homeController");
+
 const router = express.Router();
 
 router.get("/", getindex);
 
 router.get("/index", getindex);
 
-router.get("/login", getlogin);
+router.post("/check-customer", postCheckCustomer);
 
-router.get("/signin", getsignin);
-
-router.get("/support/:id", getsupport);
-
-router.get("/payment", getpayment);
-
-router.get("/intro", getintro);
-
-router.get("/results/:id", getResults);
-
-router.get("/home/:id", gethome);
-
-router.get("/admin", getadmin);
-
-router.get("/UpdateUser/:id", getUpdateUser);
-
-router.get("/UpdateProduct/:id", getUpdateProduct);
-
-router.post("/create-user", postCreateUser);
-
-router.post("/update-user", postUpdateUser);
-
-router.post("/delete-user/:id", postDeleteUser);
-
-router.post("/check-user", postCheckUser);
-
-router.post("/create-product", postCreateProduct);
-
-router.post("/update-product", postUpdateProduct);
-
-router.post("/delete-product/:id", postDeleteProduct);
-
-router.post("/search", postSearchProduct);
-
+router.post("/create-customer", postCreateCustomer);
 module.exports = router;
